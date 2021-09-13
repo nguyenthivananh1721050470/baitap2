bf30e8dce498906a988b223b20be97184dd7cd99
vanht <vanht@DESKTOP-DAH0O3O>
2021-09-13 20:44:28 +0700

    Add project files.

---
+++ baitap2/Models/BaiTapController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace baitap2.Models
+{
+    public class BaiTapController : Controller
+    {
+        
+        // GET: BaiTap
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hello()
+        {
+            return View();
+        }
+        
+        
+    }
+}
